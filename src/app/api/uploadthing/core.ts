43412eacd642b81a4137927ad1be7f32723ce98a
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-12-02 18:15:09 +0700

    adjust

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,26 +6,14 @@ import { OK_MESSAGES } from "~/trpc/shared";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "1MB" } })
-    .middleware(async () => {
-      const session = await getServerAuthSession();
-      if (!session) throw new Error("Unauthorized");
-      return { userId: session.user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId);
-      console.log("file url", file.url);
-      return { uploadedBy: metadata.userId };
-    }),
-
   uploadUserImage: f({ image: { maxFileSize: "1MB" } })
     .middleware(async () => {
       const session = await getServerAuthSession();
-      if (!session) throw new Error("Unauthorized");
+      if (!session) throw new Error("UNAUTHORIZED");
       return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const image = await db.image.create({ data: { url: file.url, name: file.name } });
+      const image = await db.document.create({ data: { url: file.url, name: file.name, ownerId: metadata.userId } });
       await db.user.update({ where: { id: metadata.userId }, data: { imageId: image.id } });
       return { message: OK_MESSAGES.OK };
     }),
