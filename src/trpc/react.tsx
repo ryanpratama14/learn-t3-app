2c329dbf623a6cdcff565d687206eaa92a22ca0f
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-12-03 23:19:54 +0700

    adjust

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -21,12 +21,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode; cookies: s
         }),
         unstable_httpBatchStreamLink({
           url: getUrl(),
-          headers() {
-            return {
-              cookie: props.cookies,
-              "x-trpc-source": "react",
-            };
-          },
+          headers: () => ({ cookie: props.cookies, "x-trpc-source": "react" }),
         }),
       ],
     })
